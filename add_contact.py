915f2a2824e55f403890bad2e344ea4a8c0eb6ad
Irina <irina@gmail.com>
2019-01-13 12:39:23 +0300

    add class contact

--- add_contact.py
+++ add_contact.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
-
+from contact import contact
 
 class AddContact(unittest.TestCase):
     def setUp(self):
@@ -16,31 +16,31 @@ class AddContact(unittest.TestCase):
     def test_add_contact(self):
         driver = self.driver
         self.login(driver, "admin", "secret")
-        self.add_contact(driver, "Ivan", "Ivanovich", "Ivanov", "ivan@ivan.ru")
+        self.add_contact(driver, contact("Ivan", "Ivanovich", "Ivanov", "ivan@ivan.ru"))
         self.logout(driver)
 
     def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
 
-    def add_contact(self, driver, name, middle_name, last_name, email):
+    def add_contact(self, driver, contact):
         # add new contact
         driver.find_element_by_link_text("add new").click()
         # add first name
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(name)
+        driver.find_element_by_name("firstname").send_keys(contact.name)
         # add middle name
         driver.find_element_by_name("middlename").click()
         driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(middle_name)
+        driver.find_element_by_name("middlename").send_keys(contact.middle_name)
         # add last name
         driver.find_element_by_name("lastname").click()
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(last_name)
+        driver.find_element_by_name("lastname").send_keys(contact.last_name)
         # add email
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(email)
+        driver.find_element_by_name("email").send_keys(contact.email)
         # submit
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
---
+++ contact.py
@@ -0,0 +1,7 @@
+class contact
+
+    def __init__(self, name, middle_name, last_name, email):
+        self.name=name
+        self.middle_name=middle_name
+        self.last_name=last_name
+        self.email = email
