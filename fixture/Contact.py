db54ffd50ba86e147ae2f5cbda7d5501095cdd94
Irina <irina@gmail.com>
2019-01-19 15:42:17 +0300

    Different areas packages are created

---
+++ fixture/Contact.py
@@ -0,0 +1,26 @@
+class ContactHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, contact):
+        wd = self.app.wd
+        # add new contact
+        wd.find_element_by_link_text("add new").click()
+        # add first name
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.name)
+        # add middle name
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        # add last name
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        # add email
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
