394cda24b741a8502a50eebf45836b641924fc82
Irina <irina@gmail.com>
2019-01-19 16:47:30 +0300

    fixture made common

--- fixture/group.py
+++ fixture/group.py
@@ -22,4 +22,14 @@ class GroupHelper:
         wd.find_element_by_link_text("groups").click()
 
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        
+
+
 
--- test/add_contact.py
+++ test/add_contact.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import contact
-from fixture.application import Application
-import pytest
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app): #тестовый метод прнимающий в качестве пвараметра фикстуру
