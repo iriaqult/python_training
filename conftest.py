231c09f28038c8e7c8918723a4f215bb586c0c6b
Irina <irina@gmail.com>
2019-01-29 14:23:26 +0300

    if-then in login

--- conftest.py
+++ conftest.py
@@ -1,8 +1,14 @@
 from fixture.application import Application
+from fixture.session import SessionHelper
 import pytest
 
 @pytest.fixture(scope= 'session')
 def app(request):
+    #global fixture
+    #if fixture is None:
     fixture = Application()
+    #else:
+    #    if not fixture
+    fixture.session.ensure_login(username="admin", password="secret")
     request.addfinalizer(fixture.destroy)
     return fixture
--- fixture/session.py
+++ fixture/session.py
@@ -18,7 +18,32 @@ class SessionHelper:
             wd.find_element_by_xpath("//input[@value='Login']").click()
 
 
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            wd.find_element_by_link_text("Logout").click()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+
+    def is_logged_in_as(self):
+        wd = self.app.wd
+        return len(wd.find_element_by_xpath("//div[@id='top']/form/b").text) = "("username")"
