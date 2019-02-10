b490744b9ba0f215fc9c766228308969e615657a
Irina <irina@gmail.com>
2019-02-10 14:20:24 +0300

    del group by index

--- fixture/group.py
+++ fixture/group.py
@@ -22,6 +22,10 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def modify_first_group(self,new_group_data):
         wd = self.app.wd
         self.open_groups_page()
@@ -38,10 +42,15 @@ class GroupHelper:
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new"))>0):
             wd.find_element_by_link_text("groups").click()
 
+
     def delete_first_group(self):
+        self. delete_group_by_index(0)
+
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.group_cache = None
