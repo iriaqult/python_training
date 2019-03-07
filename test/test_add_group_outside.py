48f742bd1e91bd0f9e24238140b4bb4c4e1f231f
Irina <irina@gmail.com>
2019-03-07 09:39:47 +0300

    18&19

--- test/test_add_group_outside.py
+++ test/test_add_group_outside.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.groupn import Groupn
-import pytest
-from data.add_group import constant as testdata
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+#import pytest
+#from data.add_group import constant as testdata
+
+#@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
